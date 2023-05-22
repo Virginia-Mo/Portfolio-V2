51552791b3187e44077bdd11e4887875f2f38eff
Virginia-Mo <antonio.vir@hotmail.com>
2023-05-22 10:45:50 +0200

    queries changed

--- utils/playerMove.js
+++ utils/playerMove.js
@@ -2,76 +2,76 @@ export function playerMove(player){
     onUpdate(() => {
         camPos(player.pos)
     })
-    if (window.screen.width < 1024) {
-            const back = add([sprite('back'), scale(2), pos(1055, 600), area(), body({
-        isStatic: true
-    }), fixed(), 'back'])
-    const next = add([sprite('next'), scale(2), pos(1185, 600), area(), body({
-        isStatic: true
-    }), fixed(), 'next'])
-    const up = add([sprite('up'), scale(2), pos(1120, 550), area(), body({
-        isStatic: true
-    }), fixed(), 'up'])
-    const down = add([sprite('down'), scale(2), pos(1120, 650), area(), body({
-        isStatic: true
-    }), fixed(), 'down'])
-        down.onClick(() => {
-        if (player.isInDialogue){ 
-            return
-        }
-        if (player.curAnim() !== 'godown'){
-            setSprite(player, 'player-down')
-            player.play('godown')
-        }
-        player.move(0,player.speed)
-    })
+    // if (window.screen.width < 1024) {
+    //         const back = add([sprite('back'), scale(2), pos(1055, 600), area(), body({
+    //     isStatic: true
+    // }), fixed(), 'back'])
+    // const next = add([sprite('next'), scale(2), pos(1185, 600), area(), body({
+    //     isStatic: true
+    // }), fixed(), 'next'])
+    // const up = add([sprite('up'), scale(2), pos(1120, 550), area(), body({
+    //     isStatic: true
+    // }), fixed(), 'up'])
+    // const down = add([sprite('down'), scale(2), pos(1120, 650), area(), body({
+    //     isStatic: true
+    // }), fixed(), 'down'])
+    //     down.onClick(() => {
+    //     if (player.isInDialogue){ 
+    //         return
+    //     }
+    //     if (player.curAnim() !== 'godown'){
+    //         setSprite(player, 'player-down')
+    //         player.play('godown')
+    //     }
+    //     player.move(0,player.speed)
+    // })
 
-    up.onClick(() => {
-        if (player.isInDialogue){ 
-            return
-        }
-        if (player.curAnim() !== 'goup'){
-            setSprite(player, 'player-up')
-            player.play('goup')
-        }
-        player.move(0,-player.speed)
-    }) 
+    // up.onClick(() => {
+    //     if (player.isInDialogue){ 
+    //         return
+    //     }
+    //     if (player.curAnim() !== 'goup'){
+    //         setSprite(player, 'player-up')
+    //         player.play('goup')
+    //     }
+    //     player.move(0,-player.speed)
+    // }) 
 
-    back.onClick(() => {
-        if (player.isInDialogue){ 
-            return
-        }
-        player.flipX = false
-        if (player.curAnim() !== 'walk'){
-            setSprite(player, 'player-side')
-            player.play('walk')
-        }
-        player.move(-player.speed, 0)
-    })
+    // back.onClick(() => {
+    //     if (player.isInDialogue){ 
+    //         return
+    //     }
+    //     player.flipX = false
+    //     if (player.curAnim() !== 'walk'){
+    //         setSprite(player, 'player-side')
+    //         player.play('walk')
+    //     }
+    //     player.move(-player.speed, 0)
+    // })
 
-    back.onClick(() => {
-        if (player.isInDialogue){ 
-            return
-        }
-        player.flipX = false
-        if (player.curAnim() !== 'walk'){
-            setSprite(player, 'player-side')
-            player.play('walk')
-        }
-        player.move(-player.speed, 0)
-    })
-    next.onClick(() => {
-        if (player.isInDialogue){ 
-            return
-        }
-        player.flipX = true
-        if (player.curAnim() !== 'walk'){
-            setSprite(player, 'player-side')
-            player.play('walk')
-        }
-        player.move(player.speed, 0)
-    })
-      }
+    // back.onClick(() => {
+    //     if (player.isInDialogue){ 
+    //         return
+    //     }
+    //     player.flipX = false
+    //     if (player.curAnim() !== 'walk'){
+    //         setSprite(player, 'player-side')
+    //         player.play('walk')
+    //     }
+    //     player.move(-player.speed, 0)
+    // })
+    // next.onClick(() => {
+    //     if (player.isInDialogue){ 
+    //         return
+    //     }
+    //     player.flipX = true
+    //     if (player.curAnim() !== 'walk'){
+    //         setSprite(player, 'player-side')
+    //         player.play('walk')
+    //     }
+    //     player.move(player.speed, 0)
+    // })
+    //   }
 
 
     function setSprite(player, spriteName){
